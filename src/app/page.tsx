a18c943cf1f05f5a552150b87e941a1bef2e77e2
jf-arce <josefranciscoarce21@gmail.com>
2025-08-19 01:00:31 -0300

    chore: add and configure shadcn UI with custom theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>PaginaWeb - Inmobiliaria Jalil</h1>
+    <div>
+      <h1 className="text-4xl">PaginaWeb - Inmobiliaria Jalil</h1>
+      <Button>Inmobiliaria Jalil</Button>
     </div>
   );
 }
