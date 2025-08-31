5e051256e1798346b6f207a7d3196a4fd6926892
jf-arce <josefranciscoarce21@gmail.com>
2025-08-31 17:15:05 -0300

    refactor: add Google Maps API key configuration and update PropiedadMap component to use it

---
+++ src/config/env.ts
@@ -0,0 +1,1 @@
+export const { NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: googleMapsApiKey } = process.env;
--- src/modules/propiedades/components/propiedad-detail/PropiedadMap.tsx
+++ src/modules/propiedades/components/propiedad-detail/PropiedadMap.tsx
@@ -1,10 +1,11 @@
 "use client";
+import { googleMapsApiKey } from "@/config/env";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 export const PropiedadMap = ({ latitud, longitud }: { latitud: number; longitud: number }) => {
 	return (
 		<div className="w-full h-[460px] rounded-xl overflow-hidden">
-			<APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
+			<APIProvider apiKey={googleMapsApiKey || ""}>
 				<Map
 					defaultZoom={18}
 					defaultCenter={{ lat: latitud, lng: longitud }}
