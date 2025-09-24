7b70c3179e355986a0c83205d7589808c5835407
jf-arce <josefranciscoarce21@gmail.com>
2025-09-24 18:17:50 -0300

    feat: add appraisal form with inputs, radios, and checkboxes on Appraisals page

--- src/app/(main)/tasaciones/page.tsx
+++ src/app/(main)/tasaciones/page.tsx
@@ -1,11 +1,88 @@
 import { PageContainer } from "@/components/layouts/PageContainer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import PhoneInput from "@/components/ui/phone-input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function TasacionesPage() {
 	return (
 		<>
-			<PageContainer>
-				<h1>Tasaciones Page</h1>
-			</PageContainer>
+			<section>
+				<PageContainer className="py-8 sm:py-10 flex flex-col sm:gap-10 gap-8 sm:!max-w-[50rem] xl:!max-w-[50rem] 2xl:!max-w-[50rem]">
+					<div className="flex flex-col gap-4">
+						<h1 className="text-3xl md:text-3xl lg:text-4xl font-semibold">Tasaciones</h1>
+						<p className="text-pretty text-sm md:text-base">
+							Le brindamos un servicio profesional de tasación de inmuebles a cargo de profesionales
+							matriculados y experimentados en el área. Nos comunicaremos con Ud. para más detalles
+							sobre la propiedad y establecer el mejor momento para realizar la tasación y asesorar
+							la compraventa.
+						</p>
+					</div>
+					<form action="">
+						<div className="flex flex-col gap-6">
+							<Input type="text" placeholder="Nombre" className="bg-white text-sm md:text-base" />
+							<RadioGroup
+								defaultValue="alquilar"
+								className="flex gap-6 sm:gap-16 justify-between sm:justify-center"
+							>
+								<div className="flex items-center gap-2">
+									<RadioGroupItem
+										value="alquilar"
+										id="alquilar"
+										className="bg-white cursor-pointer"
+									/>
+									<Label
+										htmlFor="alquilar"
+										className="font-normal cursor-pointer text-sm md:text-base"
+									>
+										Quiero <span className="text-secondary font-bold">alquilar</span> mi propiedad
+									</Label>
+								</div>
+								<div className="flex items-center gap-2">
+									<RadioGroupItem
+										value="comprar"
+										id="comprar"
+										className="bg-white cursor-pointer"
+									/>
+									<Label
+										htmlFor="comprar"
+										className="font-normal cursor-pointer text-sm md:text-base"
+									>
+										Quiero <span className="text-secondary font-bold">comprar</span> mi propiedad
+									</Label>
+								</div>
+							</RadioGroup>
+							<div className="flex flex-col gap-4">
+								<Input
+									type="text"
+									placeholder="Dirección"
+									className="bg-white text-sm md:text-base"
+								/>
+								<Input type="email" placeholder="Email" className="bg-white text-sm md:text-base" />
+								<PhoneInput name="phone" className="bg-white text-sm md:text-base" />
+								<Textarea placeholder="Mensaje" className="bg-white text-sm md:text-base" />
+							</div>
+							<div className="flex justify-between">
+								<div className="flex items-center gap-2">
+									<Checkbox className="bg-white" id="prefiero-que-me-llamen" />
+									<Label
+										htmlFor="prefiero-que-me-llamen"
+										className="font-normal cursor-pointer text-sm md:text-base"
+									>
+										Prefiero que me llamen
+									</Label>
+								</div>
+								<Button type="submit" className="text-sm md:text-base py-5 px-7">
+									Enviar
+								</Button>
+							</div>
+						</div>
+					</form>
+				</PageContainer>
+			</section>
 		</>
 	);
 }
