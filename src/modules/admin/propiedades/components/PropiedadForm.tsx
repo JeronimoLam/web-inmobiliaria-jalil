84258b5fc0b864feb9590b7fed9e153e52f317d2
jf-arce <josefranciscoarce21@gmail.com>
2025-10-08 19:15:45 -0300

    refactor: remove unused Link import in PropiedadForm component

--- src/modules/admin/propiedades/components/PropiedadForm.tsx
+++ src/modules/admin/propiedades/components/PropiedadForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormProvider } from "react-hook-form";
 import { ArrowLeft } from "lucide-react";
